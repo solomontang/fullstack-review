3de71f5b243de7268c0b0e6535b9aef3738ae2ae
Solomon Tang <solomoncztang@gmail.com>
2017-06-16 14:54:31 -0700

    Add request package

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var request = require('request');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
 app.post('/repos/import', function (req, res) {
   // TODO
   console.log('I GOT POST', req.body);
+  res.send('nice post dude');
+
 });
 
 app.get('/repos', function (req, res) {
